c72db66b836e0a5615c8c1488d3f9faa09b88112
dulshaniko7 <dulshaniko7@gmail.com>
2022-12-05 20:33:20 +0530

    g

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="getting-started-light">
+      <CronLight v-model="value" @error="error=$event"></CronLight>
+      <div class="mt-2 grey--text text--darken-1">cron expression: {{value}}</div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+
+  },
+  props: {
+    init: {
+      type: String,
+      default: '30 12,19 24 12 *'
+    }
+  },
+
+  data() {
+    return {
+      value: this.init,
+      error: ''
+    }
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//import cronLight from '@vue-js-cron/light'
+import '@vue-js-cron/light/dist/light.css'
 Vue.config.productionTip = false
 
 new Vue({
